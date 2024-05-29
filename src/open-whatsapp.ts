8aa0ce3422faef550c36448cf44b5e81b2d47db2
Aviv Ben Shahar <avivbens87@gmail.com>
2024-05-30 00:11:50 +0300

    fix: remove `open` lib - use `execPrm`

--- src/open-whatsapp.ts
+++ src/open-whatsapp.ts
@@ -1,8 +1,12 @@
 import alfy from 'alfy'
 import type { PhoneNumber } from 'libphonenumber-js'
 import { parsePhoneNumber } from 'libphonenumber-js'
-import open from 'open'
 import type { ContactPayload } from './models/contact-payload.model.js'
+import { promisify } from 'node:util'
+import { exec } from 'node:child_process'
+
+const execPrm = promisify(exec)
+
 ;(async () => {
     const rawInput: string = alfy.input
     const input: ContactPayload = JSON.parse(rawInput)
@@ -12,7 +16,8 @@ import type { ContactPayload } from './models/contact-payload.model.js'
     const { number }: PhoneNumber = parsePhoneNumber(inputPhoneNumber, inputCountryCode)
 
     const urlNew = `whatsapp://send?phone=${number}`
-    open(urlNew)
+
+    await execPrm(`open ${urlNew}`)
 
     // const url: string = `https://api.whatsapp.com/send?phone=${number}`
 
