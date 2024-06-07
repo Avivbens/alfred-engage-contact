2367bef446542a17adfab1dae88a29534d343df0
Aviv Ben Shahar <avivbens87@gmail.com>
2024-06-07 16:02:51 +0300

    feat: support `sms`, `whatsapp` and `call` features

---
+++ src/main/engage-contact.ts
@@ -0,0 +1,24 @@
+import { FastAlfred } from 'fast-alfred'
+import type { PhoneNumber } from 'libphonenumber-js'
+import { parsePhoneNumber } from 'libphonenumber-js'
+import { execPromise } from '@common/utils.js'
+import type { ContactPayload } from '@models/contact-payload.model.js'
+import { buildOpenUrl } from '@services/platform-url-builder.service.js'
+
+;(async () => {
+    const alfredClient = new FastAlfred()
+
+    try {
+        const input: ContactPayload = JSON.parse(alfredClient.input)
+
+        const { phoneNumber: inputPhoneNumber, countryCode: inputCountryCode, platform } = input
+
+        const { number }: PhoneNumber = parsePhoneNumber(inputPhoneNumber, inputCountryCode)
+
+        const openUrl = buildOpenUrl(platform, number)
+
+        await execPromise(`open ${openUrl}`)
+    } catch (error) {
+        alfredClient.error(error)
+    }
+})()
